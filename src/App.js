41a030789f5580add4c103aa754ddb32d43f291d
kellyn <kellynwong@hotmail.com>
2024-05-15 22:43:08 +0800

    Set up connect to wallet button

--- src/App.js
+++ src/App.js
@@ -10,9 +10,55 @@ import Divorce from "./components/Divorce";
 import Dispute from "./components/Dispute";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadBlockchainData = async () => {
+    // Connect to blockchain
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    setAccount(accounts[0]);
+  };
+
+  // Call loadBlockchainData function on mount
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        await loadBlockchainData();
+      } catch (error) {
+        console.error("An error has occurred: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Update ui if user changes metamask account
+  window.ethereum.on("accountsChanged", async () => {
+    // Refetch accounts
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    setAccount(accounts[0]);
+    console.log(account);
+  });
+
+  // For unconnected metamask users
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+    console.log(account);
+  };
+
   return (
     <div className="font-pixel text-sm ">
-      <DataContext.Provider value={{}}>
+      <DataContext.Provider value={{ account, setAccount, connectHandler }}>
         <div>
           <NavBar />
           <Routes>
